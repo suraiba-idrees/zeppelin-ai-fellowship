fea167953860d867f1567449543e263361de9dc7
Suraiba Idrees <suraibaidrees@gmail.com>
2026-07-24 16:43:55 +0500

    fix: link frontend to live environment variable

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
       formData.append("resume", resumeFile);
       formData.append("job_description", jobDescription);
 
-      const response = await fetch("http://127.0.0.1:8000/api/analyze", {
-      method: "POST",
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/analyze`, {
+
       body: formData,
       });
       
